45ea55c2fcd7343949aa2987c26b3e4fc5cee33e
MrMetacomCode <mrmetacomcode@gmail.com>
2020-09-13 00:20:01 -0500

    First commit

---
+++ welcome-dm.py
@@ -0,0 +1,21 @@
+import os
+
+import discord
+
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to Metacomet! Be sure to select your role in #welcome-and-roles.  Have fun here!'
+    )
+
+client.run(TOKEN)
