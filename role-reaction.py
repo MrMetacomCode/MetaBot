040ef65e2b022bb4b15f5d4055f7d530468cb1dc
MrMetacomCode <mrmetacomcode@gmail.com>
2020-09-17 21:14:57 -0500

    Just saving my progress.

--- role-reaction.py
+++ role-reaction.py
@@ -1,12 +1,20 @@
+import os
 import discord
 
-@bot.event
-async def on_ready():
-    channel = bot.get_channel('487165969903517696')
-    role = discord.utils.get(user.server.roles, name="CSGO_P")
-    message = await bot.send_message(channel, "React to me!")
-    while True:
-        reaction = await bot.wait_for_reaction(emoji="🏃", message=message)
-        await bot.add_roles(reaction.message.author, role)
-       
-bot.run
+client = discord.Client()
+
+TOKEN = os.getenv('METABOT_DISCORD_TOKEN')
+
+
+@client.event
+async def on_reaction_add(reaction, user):
+    role_channel_id = '700895165665247325'
+    if reaction.message.channel.id != role_channel_id:
+        return
+    if str(reaction.emoji) == "<:WarThunder:745425772944162907>":
+        await client.add_roles(user, name='War Thunder')
+
+
+print("Server Running")
+
+client.run(TOKEN)
