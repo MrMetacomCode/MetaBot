5a8249d7f307fb527e4996b4e7d8406e95313d67
MrMetacomCode <mrmetacomcode@gmail.com>
2020-09-18 20:34:22 -0500

    Successful role reaction assignment, working on role reaction removal when reaction is removed.

--- role-reaction.py
+++ role-reaction.py
@@ -7,13 +7,20 @@ TOKEN = os.getenv('METABOT_DISCORD_TOKEN')
 
 
 @client.event
-async def on_reaction_add(reaction, user):
-    role_channel_id = '700895165665247325'
-    if reaction.message.channel.id != role_channel_id:
-        return
-    if str(reaction.emoji) == "<:WarThunder:745425772944162907>":
-        await client.add_roles(user, name='War Thunder')
+async def on_raw_reaction_add(payload):
+    # channel and message IDs should be integer:
+    if payload.channel_id == 700895165665247325 and payload.message_id == 756577133165543555:
+        if str(payload.emoji) == "<:WarThunder:745425772944162907>":
+            role = discord.utils.get(payload.member.guild.roles, name='War Thunder')
+            await payload.member.add_roles(role)
 
+
+async def on_raw_reaction_remove(payload):
+    # channel and message IDs should be integer:
+    if payload.channel_id == 700895165665247325 and payload.message_id == 756577133165543555:
+        if str(payload.emoji) != "<:WarThunder:745425772944162907>":
+            role = discord.utils.get(payload.member.guild.roles, name='War Thunder')
+            await payload.member.remove_roles(role)
 
 print("Server Running")
 
