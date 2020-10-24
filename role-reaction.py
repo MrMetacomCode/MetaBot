9ab4f34846ea4efb8cb980e561f21eb4242e64d5
MrMetacomCode <mrmetacomcode@gmail.com>
2020-10-24 01:55:50 -0500

    Updated Discord.py and cleaned up code.

--- role-reaction.py
+++ role-reaction.py
@@ -1,8 +1,10 @@
 import os
 import discord
 from discord.utils import get
+from discord import Intents
 
-client = discord.Client()
+intents = Intents.all()
+client = discord.Client(intents=intents)
 
 TOKEN = os.getenv('METABOT_DISCORD_TOKEN')
 
@@ -36,100 +38,59 @@ async def on_message(message):
         return
 
 
-# This is to add and remove the role when the reaction is removed or added to the welcome message.
+# Assign the role when the role is added as a reaction to the message.
 @client.event
 async def on_raw_reaction_add(payload):
+    guild = client.get_guild(payload.guild_id)
+    member = get(guild.members, id=payload.user_id)
     # channel and message IDs should be integer:
     if payload.channel_id == 700895165665247325 and payload.message_id == 757114312413151272:
         if str(payload.emoji) == "<:Apex:745425965764575312>":
-            guild = client.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
             role = get(payload.member.guild.roles, name='Apex')
-            await payload.member.add_roles(role)
-            print(f"Assigned {member} {role}.")
-
-        if str(payload.emoji) == "<:WarThunder:745425772944162907>":
-            guild = client.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
+        elif str(payload.emoji) == "<:WarThunder:745425772944162907>":
             role = get(payload.member.guild.roles, name='War Thunder')
-            await payload.member.add_roles(role)
-            print(f"Assigned {member} {role}.")
-
-        if str(payload.emoji) == "<:ModernWarfare:757104623738814554>":
-            guild = client.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
+        elif str(payload.emoji) == "<:ModernWarfare:757104623738814554>":
             role = get(payload.member.guild.roles, name='Modern Warfare')
-            await payload.member.add_roles(role)
-            print(f"Assigned {member} {role}.")
-
-        if str(payload.emoji) == "<:R6Siege:757030019909550122>":
-            guild = client.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
+        elif str(payload.emoji) == "<:R6Siege:757030019909550122>":
             role = get(payload.member.guild.roles, name='R6 Siege')
-            await payload.member.add_roles(role)
-            print(f"Assigned {member} {role}.")
-
-        if str(payload.emoji) == "<:Minecraft:757029546632413346>":
-            guild = client.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
+        elif str(payload.emoji) == "<:Minecraft:757029546632413346>":
             role = get(payload.member.guild.roles, name='Minecraft')
-            await payload.member.add_roles(role)
-            print(f"Assigned {member} {role}.")
-
-        if str(payload.emoji) == "<:AmongUs:760192601625591859>":
-            guild = client.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
+        elif str(payload.emoji) == "<:AmongUs:760192601625591859>":
             role = get(payload.member.guild.roles, name='Among Us')
+        else:
+            role = get(guild.roles, name=payload.emoji)
+
+        if role is not None:
             await payload.member.add_roles(role)
-            print(f"Assigned {member} {role}.")
+            print(f"Assigned {member} to {role}.")
 
 
+# Assign the role when the role is added as a reaction to the message.
 @client.event
 async def on_raw_reaction_remove(payload):
+    guild = client.get_guild(payload.guild_id)
+    member = get(guild.members, id=payload.user_id)
     if payload.channel_id == 700895165665247325 and payload.message_id == 757114312413151272:
         if str(payload.emoji) == "<:Apex:745425965764575312>":
-            guild = client.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
             role = get(guild.roles, name='Apex')
-            await member.remove_roles(role)
-            print(f"Removed {role} from {member}.")
-
-        if str(payload.emoji) == "<:WarThunder:745425772944162907>":
-            guild = client.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
+        elif str(payload.emoji) == "<:WarThunder:745425772944162907>":
             role = get(guild.roles, name='War Thunder')
-            await member.remove_roles(role)
-            print(f"Removed {role} from {member}.")
-
-        if str(payload.emoji) == "<:ModernWarfare:757104623738814554>":
-            guild = client.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
+        elif str(payload.emoji) == "<:ModernWarfare:757104623738814554>":
             role = get(guild.roles, name='Modern Warfare')
-            await member.remove_roles(role)
-            print(f"Removed {role} from {member}.")
-
-        if str(payload.emoji) == "<:R6Siege:757030019909550122>":
-            guild = client.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
+        elif str(payload.emoji) == "<:R6Siege:757030019909550122>":
             role = get(guild.roles, name='R6 Siege')
-            await member.remove_roles(role)
-            print(f"Removed {role} from {member}.")
-
-        if str(payload.emoji) == "<:Minecraft:757029546632413346>":
-            guild = client.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
+        elif str(payload.emoji) == "<:Minecraft:757029546632413346>":
             role = get(guild.roles, name='Minecraft')
-            await member.remove_roles(role)
-            print(f"Removed {role} from {member}.")
-
-        if str(payload.emoji) == "<:AmongUs:760192601625591859>":
-            guild = client.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
+        elif str(payload.emoji) == "<:AmongUs:760192601625591859>":
             role = get(guild.roles, name='Among Us')
+        else:
+            role = discord.utils.get(guild.roles, name=payload.emoji)
+
+        if role is not None:
             await member.remove_roles(role)
             print(f"Removed {role} from {member}.")
 
 
-print("Server Running")
+print("Server Running.")
 
 client.run(TOKEN)
