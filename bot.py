0f7b9d84f74fc75092f0891beee42fc931127e4a
MrMetacomCode <mrmetacomcode@gmail.com>
2021-01-22 00:35:01 -0600

    Changed if hour is 0 again.

--- bot.py
+++ bot.py
@@ -525,7 +525,7 @@ class MetaBot(commands.Cog):
             if hour > 12:
                 hour = hour - 12
             if hour == 0:
-                hour = 1
+                hour = 12
             new_date = f"{month}/{day}/{year}"
             new_time = f"{hour}:{minute}{morning_or_night}"
             now = f"{new_date} at {new_time}"
